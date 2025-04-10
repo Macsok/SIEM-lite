d1a3d01f465b00f956bb4573730caaf70d4798d1
Macsok <skatowanypl@gmail.com>
2025-04-10 11:02:25 +0200

    Added string query

--- app.py
+++ app.py
@@ -3,31 +3,47 @@ import requests
 
 app = Flask(__name__)
 
-ELASTIC_URL = "http://localhost:9200/_search"
+ELASTIC_URL = "http://localhost:9200/loghub-logs-*/_search"
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def search():
-    query = request.args.get("q", "")
+    query = ""
     results = []
+    error = None
 
-    if query:
-        payload = {
-            "query": {
-                "multi_match": {
-                    "query": query,
-                    "fields": ["title", "content"],
-                }
+    if request.method == "POST":
+        query = request.form.get("query", "")
+        field = request.form.get("field", "").strip()
+        size = request.form.get("size", "10")
+
+        if field:
+            payload = {
+                "query": {
+                    "match_phrase": {
+                        field: query
+                    }
+                },
+                "size": int(size)
             }
-        }
+        else:
+            payload = {
+                "query": {
+                "query_string": {
+                    "query": f"*{query}*"
+                    }
+                },
+                "size": int(size)
+            }
+
         try:
             res = requests.post(ELASTIC_URL, json=payload)
             res.raise_for_status()
             data = res.json()
             results = data.get("hits", {}).get("hits", [])
         except Exception as e:
-            print("Błąd podczas zapytania do Elasticsearch:", e)
+            error = f"Error querying Elasticsearch: {e}"
 
-    return render_template("search.html", query=query, results=results)
+    return render_template("search.html", query=query, results=results, error=error)
 
 if __name__ == "__main__":
     app.run(debug=True)
