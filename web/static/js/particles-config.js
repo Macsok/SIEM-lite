02ca5618151b9336104967cedc69173a9165717b
Macsok <skatowanypl@gmail.com>
2025-05-18 01:45:47 +0200

    restyling

--- web/static/js/particles-config.js
+++ web/static/js/particles-config.js
@@ -1,17 +1,17 @@
 particlesJS("particles-js", {
   particles: {
     number: {
-      value: 60,
-      density: { enable: true, value_area: 800 }
+      value: 70,
+      density: { enable: true, value_area: 900 }
     },
-    color: { value: "#00ffcc" },
+    color: { value: ["#7b68ee", "#4d5bce", "#9370db"] },
     shape: {
       type: "circle",
       stroke: { width: 0, color: "#000000" },
       polygon: { nb_sides: 5 }
     },
     opacity: {
-      value: 0.5,
+      value: 0.4,
       random: true,
       anim: { enable: false }
     },
@@ -23,15 +23,19 @@ particlesJS("particles-js", {
     line_linked: {
       enable: true,
       distance: 150,
-      color: "#00ffcc",
-      opacity: 0.4,
+      color: "#7b68ee",
+      opacity: 0.3,
       width: 1
     },
     move: {
       enable: true,
-      speed: 1.5,
+      speed: 1.2,
       direction: "none",
-      out_mode: "out"
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 }
     }
   },
   interactivity: {
@@ -42,8 +46,9 @@ particlesJS("particles-js", {
       resize: true
     },
     modes: {
-      repulse: { distance: 100 },
-      push: { particles_nb: 4 }
+      repulse: { distance: 100, duration: 0.4 },
+      push: { particles_nb: 4 },
+      bubble: { distance: 400, size: 4, duration: 2, opacity: 0.8, speed: 3 }
     }
   },
   retina_detect: true
