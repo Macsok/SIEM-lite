68298e88d3fbaf7278f70bba0ac585633269a8a2
Julia Bajor <juliabajor@onet.pl>
2025-05-17 12:25:18 +0200

    update GUI

---
+++ static/js/particles-config.js
@@ -0,0 +1,50 @@
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 60,
+      density: { enable: true, value_area: 800 }
+    },
+    color: { value: "#00ffcc" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 }
+    },
+    opacity: {
+      value: 0.5,
+      random: true,
+      anim: { enable: false }
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false }
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#00ffcc",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 1.5,
+      direction: "none",
+      out_mode: "out"
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "repulse" },
+      onclick: { enable: true, mode: "push" },
+      resize: true
+    },
+    modes: {
+      repulse: { distance: 100 },
+      push: { particles_nb: 4 }
+    }
+  },
+  retina_detect: true
+});
